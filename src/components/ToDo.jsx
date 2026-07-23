e798e6a8acc5681da7bf950f111fcc22cac8c733
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-23 19:10:38 +0500

    feat: Input Box & Button Created in a ToDo comp

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -9,6 +9,12 @@ function ToDo() {
         <ListTodo size={25}/>
         <h1 className="text-3xl font-bold">ToDo List</h1>
       </div>
+
+      {/* Input Box  */}
+        <div className="flex items-center my-7 bg-gray-200 rounded-full">
+          <input type="text" placeholder="Add your task" className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 placeholder:text-slate-600"/>
+          <button className="bg-indigo-600 hover:bg-indigo-700 rounded-full w-32 h-14 text-white text-lg font-medium cursor-pointer">ADD +</button>
+        </div>
     </div>
   );
 }
