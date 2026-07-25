e5b8de173be9ebd11a526c0b0a4645c47c0ed8e1
AbdulMoez777 <151365220+AbdulMoez777@users.noreply.github.com>
2026-07-25 23:00:54 +0500

    feat: Implement input handling in ToDo component
    Added a reference to the input field and a function to log its value.

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,7 +1,16 @@
 import { ListTodo } from "lucide-react";
 import React from "react";
 import ToDoList from "./ToDoList";
+import {useRef} from "react"
+
 function ToDo() {
+
+  const inputRef = useRef()
+
+  const add =()=> {
+    const inputText = inputRef.current.value
+    console.log(inputText);
+  }
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl ">
       {/* title  */}
@@ -13,6 +22,7 @@ function ToDo() {
       {/* Input Box  */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           type="text"
           placeholder="Add your task"
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 placeholder:text-slate-600"
