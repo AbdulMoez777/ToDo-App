94f510d9515a83e8dff809c2d703da1951809bf4
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-26 03:26:56 +0500

    feat: Complete toggle Func

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { CircleCheck, Trash2 } from "lucide-react";
+import { Circle, CircleCheck, Trash2 } from "lucide-react";
 
-function ToDoList({ text, id, isComplete, deleteTodo }) {
+function ToDoList({ text, id, isComplete, deleteTodo, toggle }) {
   return (
     <div className="flex items-center my-3 gap-2">
-      <div className="flex flex-1 items-center cursor-pointer">
-        <CircleCheck className="w-7" />
-        <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
+      <div
+        onClick={() => {
+          toggle(id);
+        }}
+        className="flex flex-1 items-center cursor-pointer"
+      >
+        {isComplete ? (
+          <CircleCheck className="w-7 text-green-700" />
+        ) : (
+          <Circle className="w-7 text-gray-400" />
+        )}{" "}
+        <p
+          className={`ml-4 text-[17px] ${
+            isComplete ? "line-through text-gray-400" : "text-slate-700"
+          }`}
+        >
+          {text}
+        </p>
       </div>
 
       <div>
-        <Trash2 onClick={()=>{deleteTodo(id)}} size={18} className="text-slate-500 cursor-pointer" />
+        <Trash2
+          onClick={() => {
+            deleteTodo(id);
+          }}
+          size={18}
+          className="text-slate-500 cursor-pointer"
+        />
       </div>
     </div>
   );
