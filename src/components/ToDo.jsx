eaed5cf635a8df27a4ba2a5e1af58c3837cd47dc
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-26 01:33:20 +0500

    feat: Fix (add func in Input Box Button)

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,7 +1,17 @@
 import { ListTodo } from "lucide-react";
 import React from "react";
 import ToDoList from "./ToDoList";
+import { useRef } from "react";
+
 function ToDo() {
+
+  const inputRef = useRef()
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    console.log(inputText);
+    
+  }
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl ">
       {/* title  */}
@@ -13,11 +23,12 @@ function ToDo() {
       {/* Input Box  */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+        ref={inputRef}
           type="text"
           placeholder="Add your task"
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 placeholder:text-slate-600"
         />
-        <button className="bg-indigo-600 hover:bg-indigo-700 rounded-full w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button onClick={add} className="bg-indigo-600 hover:bg-indigo-700 rounded-full w-32 h-14 text-white text-lg font-medium cursor-pointer">
           ADD +
         </button>
       </div>
